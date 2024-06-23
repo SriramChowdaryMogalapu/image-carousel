25e2fdaf758dff3f983414d498b45028d706994a
SriramChowdaryMogalapu <mogalapusriram391@gmail.com>
2024-06-23 19:47:34 +0530

    image-carousel

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Carousel from "./Components/Carousel";
 import About from "./Components/About";
 import Help from "./Components/Help";
 import Bot from "./Components/Bot";
+import Contact from "./Components/Contact";
 
 const App = () => {
   const [activeTab, setActiveTab] = useState("home");
@@ -15,6 +16,8 @@ const App = () => {
     content = <About />;
   } else if (activeTab === "help") {
     content = <Help />;
+  } else if(activeTab === "contact"){
+    content = <Contact />
   } else {
     content = <Carousel />;
   }
